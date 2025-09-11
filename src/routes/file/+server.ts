d8f4873c500798e54f1bffc3c19fca8e36419a82
Kenny Ketelaar <kenny@ketelaar.tech>
2025-09-11 22:37:16 +0200

    Set a cache for the calendar retrieval

--- src/routes/file/+server.ts
+++ src/routes/file/+server.ts
@@ -6,7 +6,8 @@ export function GET() {
 	return new Response(cal.toString(), {
 		headers: {
 			'Content-Disposition': `attachment; filename=toeslagen.ics`,
-			'Content-Type': 'text/calendar; charset=utf-8'
+			'Content-Type': 'text/calendar; charset=utf-8',
+			'Cache-Control': 's-maxage=31536000, stale-while-revalidate'
 		},
 		status: 200
 	});
