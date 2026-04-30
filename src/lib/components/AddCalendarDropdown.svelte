a385b34cad0bd643a20e4f004ea45c23e57c6576
Kenny Ketelaar <kenny@ketelaar.tech>
2026-04-30 17:03:36 +0200

    Button to add to online calendar + link for Google Calendar

---
+++ src/lib/components/AddCalendarDropdown.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import Button from './ui/button/button.svelte';
+
+	type OnlineCalendarType = {
+		name: string;
+		action: () => void;
+	};
+
+	function addToAppleAgenda() {
+		console.log('Clicked on Apple Agenda');
+	}
+
+	function addToGoogleCalendar() {
+		const calendarRoute = encodeURIComponent(
+			`webcal://${window.location.host}${resolve('/toeslagen.ics')}`
+		);
+		window.open(`https://calendar.google.com/calendar/r?cid=${calendarRoute}`, '_blank');
+	}
+
+	const calendars: OnlineCalendarType[] = [
+		{ name: 'Apple Agenda', action: addToAppleAgenda },
+		{ name: 'Google Agenda', action: addToGoogleCalendar }
+	];
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger>
+		{#snippet child({ props })}
+			<Button {...props} variant="secondary">Voeg toe aan kalender</Button>
+		{/snippet}
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+		{#each calendars as { name, action } (name)}
+			<DropdownMenu.Item onclick={action}>{name}</DropdownMenu.Item>
+		{/each}
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
